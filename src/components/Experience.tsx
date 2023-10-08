10650c8a6ccc25f65e3427b02af5bde65c50dba3
Takeru9016 <youthfox046@gmail.com>
2023-10-08 20:18:07 +0530

    feat: Rough Work

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,5 +1,25 @@
 export default function Experience() {
   return (
-    <div>Experience</div>
-  )
+    <div className="flex justify-between items-center px-24 py-36">
+      <div className="inline-flex items-start gap-[60px] flex-col relative flex-[0_0_auto]">
+        <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
+          <div className="h-[34px] w-96 top-[181px] left-0 absolute bg-[#0d908b]" />
+          <p className="relative w-[620px] mt-[-1.00px] font-bold text-[#020e35] text-[54px] leading-[72px]">
+            We craft experiences for business to help reach our customers
+          </p>
+        </div>
+        <p className="w-[481px] leading-[32px] relative  font-normal text-[#020e35] text-[18px]">
+          But I must explain to you how all thismistaken idea of denouncing
+          pleasure and praising pain was born and I will give you a complete
+          account of the system.
+        </p>
+        <button className="inline-flex items-start gap-[4px] px-[40px] py-[24px] relative flex-[0_0_auto] bg-[#020e35] shadow-button all-[unset] box-border">
+          <div className="relative w-fit mt-[-1.00px] font-bold text-white text-[18px] leading-[23px] whitespace-nowrap">
+            Learn more
+          </div>
+        </button>
+      </div>
+      <div className="relative w-[780px] h-[670px] bg-[#dbeae9] rounded-[1000px]" />
+    </div>
+  );
 }
--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,5 +1,87 @@
 export default function Team() {
   return (
-    <div>Team</div>
-  )
+    <div className="flex flex-col items-center gap-[104px] px-24 py-36">
+      <div className="items-center inline-flex flex-col relative flex-[0_0_auto]">
+        <div className="w-[390px] h-[30px] top-[40px] left-[-8px] absolute bg-[#0d908b]" />
+        <div className="relative w-fit mt-[-1.00px] font-bold text-[#020e35] text-[54px] leading-[72px] whitespace-nowrap">
+          Meet the Team
+        </div>
+      </div>
+      <div className="flex flex-col items-start gap-[51px] relative self-stretch w-full flex-[0_0_auto]">
+        <div className="gap-[19px] self-stretch w-full flex-[0_0_auto] flex items-center justify-center relative">
+          <div className="flex flex-col w-[519px] h-[519px] items-center justify-center gap-[60px] px-[165px] py-[71px] relative bg-white rounded-[50px] border border-solid border-[#b7b7b7]">
+            <div className="relative w-[200px] h-[200px] ml-[-5.50px] mr-[-5.50px] bg-[#dbeae9] rounded-[100px]" />
+            <div className="inline-flex flex-col items-center gap-[24px] relative flex-[0_0_auto] ml-[-19.00px] mr-[-19.00px]">
+              <div className="relative w-fit mt-[-1.00px] font-bold text-[#020e35] text-[28px] leading-[36px] whitespace-nowrap">
+                Samantha Jones
+              </div>
+              <div className="relative w-fit font-normal text-[#020e35] text-[18px] leading-[23px] whitespace-nowrap">
+                Illustrator
+              </div>
+            </div>
+          </div>
+          <div className="flex-col w-[539px] h-[539px] gap-[60px] px-[165px] py-[71px] bg-white rounded-[50px] border border-solid border-[#b7b7b7] shadow-[0px_20px_40px_5px_#00000026] flex items-center justify-center relative">
+            <div className="w-[200px] h-[200px] rounded-[100px] relative bg-[#dbeae9]" />
+            <div className="inline-flex flex-col items-center gap-[24px] relative flex-[0_0_auto] ml-[-2.50px] mr-[-2.50px]">
+              <div className="relative w-fit mt-[-1.00px] font-bold text-[#020e35] text-[28px] leading-[36px] whitespace-nowrap">
+                Mark Ali
+              </div>
+              <div className="relative w-fit font-normal text-[#020e35] text-[18px] leading-[23px] whitespace-nowrap">
+                Owner Creative Director
+              </div>
+              <div className="font-normal text-[#020e35] relative w-fit text-[18px] leading-[23px] whitespace-nowrap">
+                See Details
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col w-[519px] h-[519px] items-center justify-center gap-[60px] px-[165px] py-[71px] relative bg-white rounded-[50px] border border-solid border-[#b7b7b7]">
+            <div className="relative w-[200px] h-[200px] ml-[-5.50px] mr-[-5.50px] bg-[#dbeae9] rounded-[100px]" />
+            <div className="inline-flex flex-col items-center gap-[24px] relative flex-[0_0_auto]">
+              <div className="relative w-fit mt-[-1.00px] font-bold text-[#020e35] text-[28px] leading-[36px] whitespace-nowrap">
+                Davi John
+              </div>
+              <div className="relative w-fit font-normal text-[#020e35] text-[18px] leading-[23px] whitespace-nowrap">
+                Head of Marketing
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="gap-[32px] self-stretch w-full flex-[0_0_auto] flex items-center justify-center relative">
+          <div className="flex-col w-[519px] h-[519px] gap-[60px] px-[165px] py-[71px] ml-[-0.50px] bg-white rounded-[50px] border border-solid border-[#b7b7b7] flex items-center justify-center relative">
+            <div className="relative w-[200px] h-[200px] ml-[-5.50px] mr-[-5.50px] bg-[#dbeae9] rounded-[100px]" />
+            <div className="inline-flex flex-col items-center gap-[24px] relative flex-[0_0_auto]">
+              <div className="relative w-fit mt-[-1.00px] font-bold text-[#020e35] text-[28px] leading-[36px] whitespace-nowrap">
+                John Daen
+              </div>
+              <div className="relative w-fit font-normal text-[#020e35] text-[18px] leading-[23px] whitespace-nowrap">
+                Graphic Designer
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col w-[519px] h-[519px] items-center justify-center gap-[60px] px-[165px] py-[71px] relative bg-white rounded-[50px] border border-solid border-[#b7b7b7]">
+            <div className="relative w-[200px] h-[200px] ml-[-5.50px] mr-[-5.50px] bg-[#dbeae9] rounded-[100px]" />
+            <div className="inline-flex flex-col items-center gap-[24px] relative flex-[0_0_auto] ml-[-3.50px] mr-[-3.50px]">
+              <div className="relative w-fit mt-[-1.00px] font-bold text-[#020e35] text-[28px] leading-[36px] whitespace-nowrap">
+                Alice Kate
+              </div>
+              <div className="relative w-fit font-normal text-[#020e35] text-[18px] leading-[23px] whitespace-nowrap">
+                Social Media Manager
+              </div>
+            </div>
+          </div>
+          <div className="flex-col w-[519px] h-[519px] gap-[60px] px-[165px] py-[71px] mr-[-0.50px] bg-white rounded-[50px] border border-solid border-[#b7b7b7] flex items-center justify-center relative">
+            <div className="relative w-[200px] h-[200px] ml-[-5.50px] mr-[-5.50px] bg-[#dbeae9] rounded-[100px]" />
+            <div className="inline-flex flex-col items-center gap-[24px] relative flex-[0_0_auto] ml-[-23.00px] mr-[-23.00px]">
+              <div className="relative w-fit mt-[-1.00px] font-bold text-[#020e35] text-[28px] leading-[36px] whitespace-nowrap">
+                Jhang Yui
+              </div>
+              <div className="relative w-fit font-normal text-[#020e35] text-[18px] leading-[23px] whitespace-nowrap">
+                Web Designer &amp; Developer
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
